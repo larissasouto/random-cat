935473a2d7c1f8b6b3b1c9e5fe686b59377b8f1c
Larissa Souto <larissa@redes.ufsm.br>
2020-08-28 11:09:07 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
-  return (
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hellooow!</h1>
+//     </div>
+//   );
+// }
+
+const App = () => {
+  const [cat, setCat] = useState('');
+  const [done, setDone] = useState(undefined);
+
+  useEffect(() => {
+    getCat();
+  }, []);
+
+  const getCat = () => {
+    setDone(undefined);
+    setTimeout(() => {
+      fetch('https://aws.random.cat/meow')
+        .then(res => res.json())
+        .then(data => {
+          setCat(data.file); //data.file -> traz imagem de gatos
+          setDone(true);
+        });
+
+    }, 1200);
+  }
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Random Cat Image Generator!</h2>
+      <button onClick={getCat}>Get Cat</button>
+      <div className="cat-pic">
+        {!done ? (
+          <p>Loading...</p>
+        ) : (
+          <img src={cat} alt="cat"/>
+        )}
+      </div>
     </div>
   );
+
 }
 
 export default App;
